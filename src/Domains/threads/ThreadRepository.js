54eed3c85e4d721e8615521e8326c20e63d64c3c
unknown <yyongkisetiawan@gmail.com>
2024-05-14 09:55:47 +0700

    Memperbaiki threads Domains

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,6 +1,6 @@
 class ThreadRepository {
   async addThread(newThread) {
-    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTD');
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
 
