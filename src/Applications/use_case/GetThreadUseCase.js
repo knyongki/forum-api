90cc91954478058055d2d01cab83d659892f40f6
unknown <yyongkisetiawan@gmail.com>
2024-05-15 18:46:27 +0700

    membuat comment dan replay usecase

---
+++ src/Applications/use_case/GetThreadUseCase.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-shadow */
+class getThreadUseCase {
+  constructor({ threadRepository, commentRepository, likeRepository }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+    this._likeRepository = likeRepository;
+  }
+
+  async execute(useCaseParam) {
+    const { threadId } = useCaseParam;
+    const threadDetail = await this._threadRepository.getThreadById(threadId);
+    threadDetail.comments = await this._commentRepository.getCommentsByThreadId(threadId);
+    const threadReplies = await this._threadRepository.getRepliesByThreadId(threadId);
+
+    threadDetail.comments = this._checkIsDeletedComments(threadDetail.comments);
+    threadDetail.comments = this._getRepliesForComments(threadDetail.comments, threadReplies);
+    threadDetail.comments = await this._getLikeCountForComments(threadDetail.comments);
+
+    return threadDetail;
+  }
+
+  _checkIsDeletedComments(comments) {
+    for (let i = 0; i < comments.length; i += 1) {
+      comments[i].content = comments[i].isDeleted ? '**komentar telah dihapus**' : comments[i].content;
+      delete comments[i].isDeleted;
+    }
+    return comments;
+  }
+
+  _getRepliesForComments(comments, threadReplies) {
+    for (let i = 0; i < comments.length; i += 1) {
+      const commentId = comments[i].id;
+      comments[i].replies = threadReplies
+        .filter((reply) => reply.commentId === commentId)
+        .map((reply) => {
+          const { commentId, ...replyDetail } = reply;
+          replyDetail.content = replyDetail.isDeleted ? '**balasan telah dihapus**' : replyDetail.content;
+          delete replyDetail.isDeleted;
+          return replyDetail;
+        });
+    }
+    return comments;
+  }
+
+  async _getLikeCountForComments(comments) {
+    for (let i = 0; i < comments.length; i += 1) {
+      const commentId = comments[i].id;
+      comments[i].likeCount = await this._likeRepository
+        .getLikeCountByCommentId(commentId);
+    }
+    return comments;
+  }
+}
+
+module.exports = getThreadUseCase;
---
+++ src/Applications/use_case/_test/GetThreadUseCase.test.js
@@ -0,0 +1,291 @@
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const DetailThread = require('../../../Domains/threads/entities/DetailThread');
+const DetailComment = require('../../../Domains/comments/entities/DetailComment');
+const DetailReply = require('../../../Domains/replies/entities/DetailReply');
+const GetThreadUseCase = require('../GetThreadUseCase');
+
+describe('GetThreadDetailUseCase', () => {
+  it('should orchestrate the get thread detail action correctly', async () => {
+    // arrange
+    const useCaseParam = {
+      threadId: 'thread-123',
+    };
+
+    const expectedDetailThread = new DetailThread({
+      id: 'thread-1234',
+      title: 'some thread title',
+      body: 'some thread body',
+      date: '2020',
+      username: 'John Doe',
+      comments: [],
+    });
+
+    const retrievedComments = [
+      new DetailComment({
+        id: 'comment-123',
+        username: 'user A',
+        date: '2021',
+        content: 'comment A',
+        replies: [],
+        isDeleted: false,
+        likeCount: 0,
+      }),
+      new DetailComment({
+        id: 'comment-456',
+        username: 'user B',
+        date: '2020',
+        content: 'comment B',
+        replies: [],
+        isDeleted: false,
+        likeCount: 0,
+      }),
+    ];
+
+    const retrievedReplies = [
+      new DetailReply({
+        id: 'reply-123',
+        commentId: 'comment-123',
+        content: 'reply A',
+        date: '2021',
+        username: 'user C',
+        isDeleted: false,
+      }),
+      new DetailReply({
+        id: 'reply-456',
+        commentId: 'comment-456',
+        content: 'reply B',
+        date: '2021',
+        username: 'user D',
+        isDeleted: false,
+      }),
+    ];
+
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+
+    mockThreadRepository.getThreadById = jest.fn()
+      .mockImplementation(() => Promise.resolve(expectedDetailThread));
+    mockThreadRepository.getRepliesByThreadId = jest.fn()
+      .mockImplementation(() => Promise.resolve(retrievedReplies));
+
+    mockCommentRepository.getCommentsByThreadId = jest.fn()
+      .mockImplementation(() => Promise.resolve(retrievedComments));
+
+    const getThreadDetailUseCase = new GetThreadUseCase({
+      threadRepository: mockThreadRepository,
+      commentRepository: mockCommentRepository,
+      likeRepository: {},
+    });
+
+    // filtering retrievedComments to remove isDeleted and likeCount
+    const {
+      isDeleted: isDeletedCommentA,
+      ...filteredCommentDetailsA
+    } = retrievedComments[0];
+    const {
+      isDeleted: isDeletedCommentB,
+      ...filteredCommentDetailsB
+    } = retrievedComments[1];
+
+    // filtering retrievedReplies to removed commentId and isDeleted
+    const {
+      commentId: commentIdReplyA, isDeleted: isDeletedReplyA,
+      ...filteredReplyDetailsA
+    } = retrievedReplies[0];
+    const {
+      commentId: commentIdReplyB, isDeleted: isDeletedReplyB,
+      ...filteredReplyDetailsB
+    } = retrievedReplies[1];
+
+    const expectedCommentsAndReplies = [
+      { ...filteredCommentDetailsA, replies: [filteredReplyDetailsA] },
+      { ...filteredCommentDetailsB, replies: [filteredReplyDetailsB] },
+    ];
+
+    getThreadDetailUseCase._checkIsDeletedComments = jest.fn()
+      .mockImplementation(() => [filteredCommentDetailsA, filteredCommentDetailsB]);
+
+    getThreadDetailUseCase._getRepliesForComments = jest.fn()
+      .mockImplementation(() => expectedCommentsAndReplies);
+
+    getThreadDetailUseCase._getLikeCountForComments = jest.fn()
+      .mockImplementation(() => Promise.resolve(expectedCommentsAndReplies));
+
+    // action
+    const useCaseResult = await getThreadDetailUseCase.execute(useCaseParam);
+
+    // assert
+
+    expect(useCaseResult).toEqual(new DetailThread({
+      ...expectedDetailThread, comments: expectedCommentsAndReplies,
+    }));
+    expect(mockThreadRepository.getThreadById).toBeCalledWith(useCaseParam.threadId);
+    expect(mockCommentRepository.getCommentsByThreadId).toBeCalledWith(useCaseParam.threadId);
+    expect(mockThreadRepository.getRepliesByThreadId).toBeCalledWith(useCaseParam.threadId);
+
+    expect(getThreadDetailUseCase._checkIsDeletedComments).toBeCalledWith(retrievedComments);
+    expect(getThreadDetailUseCase._getRepliesForComments)
+      .toBeCalledWith([filteredCommentDetailsA, filteredCommentDetailsB], retrievedReplies);
+    expect(getThreadDetailUseCase._getLikeCountForComments)
+      .toBeCalledWith(expectedCommentsAndReplies);
+  });
+
+  it('should operate the branching in the _checkIsDeletedComments function properly', () => {
+    // arrange
+    const getThreadDetailUseCase = new GetThreadUseCase(
+      { threadRepository: {}, commentRepository: {}, likeRepository: {} },
+    );
+    const retrievedComments = [
+      new DetailComment({
+        id: 'comment-123',
+        username: 'user A',
+        date: '2021',
+        content: 'comment A',
+        replies: [],
+        isDeleted: true,
+        likeCount: 0,
+      }),
+      new DetailComment({
+        id: 'comment-456',
+        username: 'user B',
+        date: '2020',
+        content: 'comment B',
+        replies: [],
+        isDeleted: false,
+        likeCount: 0,
+      }),
+    ];
+    const {
+      isDeleted: isDeletedCommentA,
+      ...filteredCommentDetailsA
+    } = retrievedComments[0];
+    const {
+      isDeleted: isDeletedCommentB,
+      ...filteredCommentDetailsB
+    } = retrievedComments[1];
+    const SpyCheckIsDeletedComments = jest.spyOn(getThreadDetailUseCase, '_checkIsDeletedComments');
+
+    // action
+    getThreadDetailUseCase._checkIsDeletedComments(retrievedComments);
+
+    // assert
+    expect(SpyCheckIsDeletedComments)
+      .toReturnWith([
+        { ...filteredCommentDetailsA, content: '**komentar telah dihapus**' },
+        filteredCommentDetailsB]);
+
+    SpyCheckIsDeletedComments.mockClear();
+  });
+
+  it('should operate the branching in the _getRepliesForComments function properly', () => {
+    // arrange
+    const getThreadDetailUseCase = new GetThreadUseCase(
+      { threadRepository: {}, commentRepository: {}, likeRepository: {} },
+    );
+    const filteredComments = [
+      {
+        id: 'comment-123',
+        username: 'user A',
+        date: '2021',
+        content: '**komentar telah dihapus**',
+        replies: [],
+        likeCount: 0,
+      },
+      {
+        id: 'comment-456',
+        username: 'user B',
+        date: '2020',
+        content: 'comment B',
+        replies: [],
+        likeCount: 0,
+      },
+    ];
+
+    const retrievedReplies = [
+      new DetailReply({
+        id: 'reply-123',
+        commentId: 'comment-123',
+        content: 'reply A',
+        date: '2021',
+        username: 'user C',
+        isDeleted: true,
+      }),
+      new DetailReply({
+        id: 'reply-456',
+        commentId: 'comment-456',
+        content: 'reply B',
+        date: '2021',
+        username: 'user D',
+        isDeleted: false,
+      }),
+    ];
+
+    const {
+      commentId: commentIdReplyA, isDeleted: isDeletedReplyA,
+      ...filteredReplyDetailsA
+    } = retrievedReplies[0];
+    const {
+      commentId: commentIdReplyB, isDeleted: isDeletedReplyB,
+      ...filteredReplyDetailsB
+    } = retrievedReplies[1];
+
+    const expectedCommentsAndReplies = [
+      { ...filteredComments[0], replies: [{ ...filteredReplyDetailsA, content: '**balasan telah dihapus**' }] },
+      { ...filteredComments[1], replies: [filteredReplyDetailsB] },
+    ];
+
+    const SpyGetRepliesForComments = jest.spyOn(getThreadDetailUseCase, '_getRepliesForComments');
+
+    // action
+    getThreadDetailUseCase
+      ._getRepliesForComments(filteredComments, retrievedReplies);
+
+    // assert
+    expect(SpyGetRepliesForComments)
+      .toReturnWith(expectedCommentsAndReplies);
+
+    SpyGetRepliesForComments.mockClear();
+  });
+
+  it('should operate the _getLikeCountForComments function properly', async () => {
+    // arrange
+    const commentsParam = [
+      {
+        id: 'comment-123',
+        username: 'user A',
+        date: '2021',
+        content: '**komentar telah dihapus**',
+        replies: [{
+          id: 'reply-123',
+          content: 'reply A',
+          date: '2021',
+          username: 'user C',
+        }],
+        likeCount: 0,
+      },
+      {
+        id: 'comment-456',
+        username: 'user B',
+        date: '2020',
+        content: 'comment B',
+        replies: [{
+          id: 'reply-456',
+          content: 'reply B',
+          date: '2021',
+          username: 'user D',
+        }],
+        likeCount: 0,
+      },
+    ];
+
+    const expectedComments = [
+      {
+        ...commentsParam[0], likeCount: 2,
+      },
+      {
+        ...commentsParam[1], likeCount: 0,
+      },
+    ];
+  });
+});
